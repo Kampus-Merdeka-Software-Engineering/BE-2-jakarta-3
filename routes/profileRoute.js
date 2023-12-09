8dfacb0e2da9bb85952197e9c93bddc4260442e8
anmuhammadakbar25 <exeaccrabka@gmail.com>
2023-12-09 16:59:39 +0800

    update be

--- routes/profileRoute.js
+++
@@ -1,9 +0,0 @@
-// routes/api.js
-const express = require('express');
-const router = express.Router();
-const controllers = require('../controllers/controller');
-
-router.get('/data', controllers.getAllData);
-router.post('/data', controllers.createData);
-
-module.exports = router;
